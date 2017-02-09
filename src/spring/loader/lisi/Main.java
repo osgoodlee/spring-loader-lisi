22576d6dfd0b8220f97f37266ec325e4e9b90ef9
Lee <shannonlee10@gmail.com>
2017-02-09 09:37:08 +0800

    第一版

--- src/spring/loader/lisi/Main.java
+++ src/spring/loader/lisi/Main.java
@@ -1,5 +1,139 @@
 package spring.loader.lisi;
 
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.UrlResource;
+
 public class Main {
 
+	@SuppressWarnings({ "resource" })
+	public static void main(String[] args){  
+//        ApplicationContext context = new ClassPathXmlApplicationContext("classpath*:/spring1.xml");  
+//        PluginLoader testLoader = (PluginLoader) context.getBean("pluginLoader");
+//        testLoader.load();
+        
+        
+        try {  
+        	//保存当前类加载器
+        	ClassLoader currentClassLoader = Thread.currentThread().getContextClassLoader();
+        	System.out.println("当前线程CL:"+currentClassLoader.toString());
+        	
+        	ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(  
+            		"classpath*:/spring1.xml");  
+        	
+//        	   Object sdf = applicationContext.getBean("hibernateSqlDao");  
+//               tryInvoke(sdf);  
+//               System.out.println(sdf.getClass().toString());
+//               
+//               CollectService dfdf = (CollectService) applicationContext.getBean("collectService");  
+//               dfdf.uploadTransactionReport();
+//               System.out.println(dfdf.getClass().toString());
+            
+//            PluginLoader testLoader = (PluginLoader) applicationContext.getBean("pluginLoader");
+//            testLoader.load();
+//            CollectService pluginBean2 = (CollectService) applicationContext.getBean("collectService");  
+//   		 	pluginBean2.uploadTransactionReport();
+//            System.out.println(pluginBean2.getClass().toString());
+            
+//            HibernateSqlDao pluginBean3 = (HibernateSqlDao) applicationContext.getBean("hibernateSqlDao");  
+//            pluginBean3.getSessionFactory();
+//            System.out.println(pluginBean3.getClass().toString());
+           
+  
+            DefaultListableBeanFactory beanFactory = (DefaultListableBeanFactory) applicationContext  
+                    .getAutowireCapableBeanFactory();  
+            
+            // 以下这行设置BeanFactory的ClassLoader，以加载外部类  
+            setBeanClassLoader(beanFactory);  
+   
+//            String configurationFilePath = "jar:file:/E:/ncr-connectors-dibs-tms-service-dibs.jar!/module.xml";  
+//            URL url = new URL(configurationFilePath);  
+//            UrlResource urlResource = new UrlResource(url);  
+//            DefaultListableBeanFactory xbf = new XmlBeanFactory(urlResource);  
+//            String[] beanIds = xbf.getBeanDefinitionNames();  
+//            for (String beanId : beanIds) {  
+//                BeanDefinition bd = xbf.getMergedBeanDefinition(beanId);  
+////                System.out.println(bd.getBeanClassName());
+//                beanFactory.registerBeanDefinition(beanId, bd);  
+//            }  
+            
+            System.out.println("当前线程CL:"+Thread.currentThread().getContextClassLoader().toString());
+            System.out.println("当前上下文CL:"+applicationContext.getClassLoader().toString());
+            
+//            beanFactory.setBeanClassLoader(currentClassLoader);  
+//            for (String beanId : applicationContext.getBeanDefinitionNames()) {  
+//                System.out.println(beanId);
+//            }  
+            System.out.println("当前上下文CL:"+applicationContext.getClassLoader().toString());
+            System.out.println("当前工厂CL:"+((DefaultListableBeanFactory)applicationContext.getAutowireCapableBeanFactory()).getBeanClassLoader().toString());
+
+            
+//            Object hibernateSqlDao = applicationContext.getBean("hibernateSqlDao");  
+//            tryInvoke(hibernateSqlDao);  
+//            System.out.println(hibernateSqlDao.getClass().toString());
+//            
+//            beanFactory.setBeanClassLoader(currentClassLoader);
+//            System.out.println("当前工厂CL:"+((DefaultListableBeanFactory)applicationContext.getAutowireCapableBeanFactory()).getBeanClassLoader().toString());
+//
+//            Object t2 = applicationContext.getBean("hibernateSqlDao");  
+//            tryInvoke(t2);  
+//            System.out.println(t2.getClass().toString());
+            
+            Object t33 = applicationContext.getBean("collectService");  
+            tryInvokeCollect(t33);  
+            System.out.println(t33.getClass().toString());
+          
+  
+        } catch (Exception exc) {  
+            exc.printStackTrace();  
+        }  
+    }  
+	
+    private static void setBeanClassLoader(  
+            DefaultListableBeanFactory beanFactory)  
+            throws MalformedURLException {  
+        String jarFilePath = "E://ncr-connectors-dibs-tms-service-dibs.jar";  
+        URL jarUrl = new File(jarFilePath).toURI().toURL();  
+        URL[] urls = new URL[] { jarUrl };  
+        URLClassLoader cl = new URLClassLoader(urls);  
+        
+        Thread.currentThread().setContextClassLoader(cl.getParent().getParent());
+        beanFactory.setBeanClassLoader(cl.getParent().getParent());  
+        
+    }  
+  
+    private static void tryInvoke(Object bean) throws SecurityException,  
+            NoSuchMethodException, IllegalArgumentException,  
+            IllegalAccessException, InvocationTargetException {  
+  
+        Class<?> paramTypes[] = new Class[0];  
+        Method method = bean.getClass()  
+                .getDeclaredMethod("getSessionFactory", paramTypes);  
+        Object paramValues[] = new Object[0];  
+        method.invoke(bean, paramValues);  
+  
+    }  
+    
+	private static void tryInvokeCollect(Object bean) throws SecurityException,
+			NoSuchMethodException, IllegalArgumentException,
+			IllegalAccessException, InvocationTargetException {
+
+		Class<?> paramTypes[] = new Class[0];
+		Method method = bean.getClass().getDeclaredMethod("uploadTransactionReport",
+				paramTypes);
+		Object paramValues[] = new Object[0];
+		method.invoke(bean, paramValues);
+
+	}
+    
 }
--- src/spring/loader/lisi/SpringFileLoader.java
+++ src/spring/loader/lisi/SpringFileLoader.java
@@ -17,35 +17,25 @@ import org.springframework.web.context.support.XmlWebApplicationContext;
 
 public class SpringFileLoader {
 
-	public static boolean loadJarFile(String jarFileUrl, XmlWebApplicationContext context) {
+	public static boolean loadJarFile(String jarFileUrl, String packageName, XmlWebApplicationContext context) {
 		try {
-			String packageName = "moduletest";
 			//如果无法成功加载jar包，退出
 			if (!loadToClassLoader(jarFileUrl)) {
 				return false;
 			}
 			// 注册注解方式的bean
 			//获取web应用正使用的bean容器，jar包中的bean将被注入到容器中
-			DefaultListableBeanFactory defaultListableBeanFactory = (DefaultListableBeanFactory) context
-					.getAutowireCapableBeanFactory();
+			DefaultListableBeanFactory defaultListableBeanFactory = (DefaultListableBeanFactory) context.getAutowireCapableBeanFactory();
 			//创建Spring的注解自动扫描器(构造函数传入容器，扫描后的bean注入该容器)，用来获取基于注解的bean，扫描器需要指定要扫描的包名，扫描jar中的包路径，使用通配符，
 			//另外在导出jar时必须选择add directory entries（即把目录也加入到jar中），否则spring扫描时将无法找到class
 			ClassPathBeanDefinitionScanner scanner = new ClassPathBeanDefinitionScanner(defaultListableBeanFactory);
-//			System.out.println(defaultListableBeanFactory.getBeanDefinitionCount());
 			//开始扫描
 			scanner.scan(packageName);
-//			System.out.println(defaultListableBeanFactory.getBeanDefinitionCount());
 			
 			// 注册xml配置中的bean到根上下文
-			String configurationFilePath = "jar:file:/" + jarFileUrl + "!/applicationContext.xml";
+			String configurationFilePath = "jar:file://" + jarFileUrl + "!/resources/applicationContext.xml";
 			URL url = new URL(configurationFilePath);
 			UrlResource urlResource = new UrlResource(url);
-//			DefaultListableBeanFactory xmlBeanFactory = new XmlBeanFactory(urlResource);
-//			String[] beanIds = xmlBeanFactory.getBeanDefinitionNames();
-//			for (String beanId : beanIds) {
-//				BeanDefinition bd = xmlBeanFactory.getMergedBeanDefinition(beanId);
-//				defaultListableBeanFactory.registerBeanDefinition(beanId, bd);
-//			}
 			//创建一个DefaultListableBeanFactory容器，用于获取applicationContext.xml文件中配置的bean
 			DefaultListableBeanFactory xmlBeanFactory = new DefaultListableBeanFactory();
 			//用该DefaultListableBeanFactory容器创建一个XmlBeanDefinitionReader，用于加载bean配置
@@ -62,34 +52,31 @@ public class SpringFileLoader {
 			
 			//获取SpringMVC中的HandlerMapping处理器，它也是一个bean．HandlerMapping将会把请求映射为HandlerExecutionChain对象
 			//（包含一个Handler处理器（页面控制器）对象、多个HandlerInterceptor拦截器）对象
-			ManualAnnotationHandlerMapping handlerMapping = context.getBean(ManualAnnotationHandlerMapping.class);
-			//如果handlerMapping不为空，表明是SpringMVC项目，处理新加jar包中的映射关系到handlerMapping中
-			if (null != handlerMapping) {
-				String[] beansName = defaultListableBeanFactory.getBeanDefinitionNames();
-				//遍历容器中所有bean
-				for (String beanName : beansName) {
-					//获取bean对应的映射url
-					String[] mappedURLs = handlerMapping.determineUrlsForHandlerByManual(beanName);
-					//映射url不为空，表明该bean是controller类中的响应方法，把该bean和对应映射url的关系写入handlerMapping
-					if (!ObjectUtils.isEmpty(mappedURLs)) {
-						handlerMapping.registerHandlerByManual(mappedURLs, beanName);
+			ManualAnnotationHandlerMapping handlerMapping;
+			try {
+				handlerMapping = context.getBean(ManualAnnotationHandlerMapping.class);
+				//如果handlerMapping不为空，表明是SpringMVC项目，处理新加jar包中的映射关系到handlerMapping中
+				if (null != handlerMapping) {
+					String[] beansName = defaultListableBeanFactory.getBeanDefinitionNames();
+					//遍历容器中所有bean
+					for (String beanName : beansName) {
+						//获取bean对应的映射url
+						String[] mappedURLs = handlerMapping.determineUrlsForHandlerByManual(beanName);
+						//映射url不为空，表明该bean是controller类中的响应方法，把该bean和对应映射url的关系写入handlerMapping
+						if (!ObjectUtils.isEmpty(mappedURLs)) {
+							handlerMapping.registerHandlerByManual(mappedURLs, beanName);
+						}
 					}
 				}
+			} catch (Exception e) {
+				//无法获取ManualAnnotationHandlerMapping,表明主项目不是springmvc
 			}
-			System.out.println(defaultListableBeanFactory.getBeanDefinitionCount());
-			// 手动拿jar包里的bean的实例时因为存在ClassLoader的隔离虽然在开始设置了ContextClassLoader
-			// 但是Spring默认getBean的时候并没有每次都去拿最新的ContextClassLoader使用，所以需要手动设置Bean的ClassLoader
-			// 因为是手动设置的这里存在线程安全的问题...不知道有没有其他方法.
-			// DefaultListableBeanFactory factory =
-			// (DefaultListableBeanFactory)context.getAutowireCapableBeanFactory();
-			// factory.setBeanClassLoader(loader);
-			
-			
+		
 //			System.out.println(context.getBean("collectController"));
-//			System.out.println(context.getBean("house"));
 
 			return true;
 		} catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
 		return false;
 	}
